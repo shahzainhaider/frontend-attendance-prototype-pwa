2a617acb4a75dd6a24f744350af9d537cbafa0f3
Anus Shafi <anushashmani10@gmail.com>
2024-07-08 22:36:09 +0500

    student dashoboard update

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -10,10 +10,10 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-// import StudentSideBar from "./StudentSideBar";
+import StudentSideBar from "./StudentSideBar";
 import { Navigate, Route, Routes } from "react-router-dom";
 import StudentHomePage from "./StudentHomePage";
-// import StudentProfile from "./StudentProfile";
+import StudentProfile from "./StudentProfile";
 // import StudentSubjects from "./StudentSubjects";
 // import ViewStdAttendance from "./ViewStdAttendance";
 // import StudentComplain from "./StudentComplain";
@@ -78,7 +78,7 @@ const StudentDashboard = () => {
             <Route path="/" element={<StudentHomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
             <Route path="/Student/dashboard" element={<StudentHomePage />} />
-            {/* <Route path="/Student/profile" element={<StudentProfile />} /> */}
+            <Route path="/Student/profile" element={<StudentProfile />} />
 
             {/* <Route path="/Student/subjects" element={<StudentSubjects />} />
             <Route path="/Student/attendance" element={<ViewStdAttendance />} />
