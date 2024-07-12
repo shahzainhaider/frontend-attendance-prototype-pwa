f48a530effaddbc18d7c1924e1481c4d5f1216f6
Muhammad-Farasat <farasatkhan687@gmail.com>
2024-07-12 11:04:50 -0700

    commit

--- src/pages/student/StudentHomePage.jsx
+++ src/pages/student/StudentHomePage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BarChart } from '@mui/x-charts/BarChart';
 
 function StudentHomePage() {
-  return <div>StudentHomePage</div>;
+  return(
+  <>
+     <BarChart
+      xAxis={[{ scaleType: 'band', data: ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] }]}
+      series={[{ data: [20, 10, 30, 20, 40, 30, 25, 60, 0, 22, 32, 12] }]}
+      width={1200}
+      height={700} />
+  </>
+)
 }
 
 export default StudentHomePage;
